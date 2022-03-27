b667863290ff147760cc6f5fd586e76305fe1863
Amindlog <amind@bk.ru>
2022-03-27 14:18:53 +0400

    init

---
+++ Client/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.Serialization;
+var isExecute = true;
+
+
+while (isExecute)
+{
+    System.Console.WriteLine("Выберите действия: ");
+    System.Console.WriteLine("Метод Ping");
+    System.Console.WriteLine("Метод GetInputData");
+    System.Console.WriteLine("Метод WriteAnswer");
+    System.Console.WriteLine("Метод Exit");
+    System.Console.WriteLine();
+    System.Console.Write("Введите метод:");
+
+    try
+    {
+        var inInputUsers = Console.ReadLine();
+
+        switch (inInputUsers.ToLower())
+        {
+            case "ping":
+                RunMethod.Ping();
+                System.Console.WriteLine("Ping");
+                break;
+            case "getinputdata":
+                // RunMethod.GetInputData();
+                System.Console.WriteLine("GetInputData");
+                System.Console.WriteLine();
+                break;
+            case "writeanswer":
+                // RunMethod.WriteAnswer();
+                System.Console.WriteLine("WriteAnswer");
+                System.Console.WriteLine();
+                break;
+            case "exit":
+                System.Console.WriteLine("Exit");
+                System.Console.WriteLine();
+                break;
+            default:
+                System.Console.Write("Что то пошло не так, введи правельный метод, \n");
+                break;
+        }
+    }
+    catch (ArgumentNullException ex)
+    {
+        System.Console.WriteLine(ex.Message);
+    }
+}
+
+public class RunMethod
+{
+    public static void Ping()
+    {
+        using (HttpClient client = new HttpClient())
+        {
+            var response = client.GetAsync("https://github.com/").Result;
+            response.StatusCode.GetHashCode();
+            var body = response.Content.ReadAsStringAsync().Result;
+            System.Console.WriteLine(body);
+        }
+    }
+
+    public static void GetInputData()
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static void WriteAnswer()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+var httpListener = new HttpListener();
+
+httpListener.Prefixes.Add("http://localhost:23234/");
+httpListener.Start();
+var IsStarted = true;
+var count = 0;
+while (IsStarted)
+{
+    var context = httpListener.GetContext();
+    var request = context.Request;
+
+
+    var response = context.Response;
+    string responseStr = $"<html><head><meta charset='utf8'></head><body>Привет мир! {count}</body></html>";
+    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseStr);
+
+    response.ContentLength64 = buffer.Length;
+    var output = response.OutputStream;
+    output.Write(buffer, 0, buffer.Length);
+
+    output.Close();
+    count++;
+}
+httpListener.Stop();
+Console.WriteLine("Обработка подключений завершена");
+Console.ReadKey();
